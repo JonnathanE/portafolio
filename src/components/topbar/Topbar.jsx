41a74ee052d9bdf7bd78fb5530cd86cd87368aa5
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-11-17 19:36:40 -0500

    React Animated Hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,27 +1,42 @@
 import { Mail, Person } from '@material-ui/icons'
 import React from 'react'
+import PropTypes from 'prop-types'
 import './topbar.scss'
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className='topbar'>
+        <div className={'topbar ' + (menuOpen && 'active')}>
             <div className="wrapper">
                 <div className="left">
                     <a href='#intro' className='logo'>JEDE</a>
                     <div className="itemContainer">
-                        <Person className='icon'/>
+                        <Person className='icon' />
                         <span>+507 924 12 74</span>
                     </div>
                     <div className="itemContainer">
-                        <Mail className='icon'/>
+                        <Mail className='icon' />
                         <span>ldsjfs@jede.com</span>
                     </div>
                 </div>
                 <h1>Hello</h1>
-                <div className="right">right</div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
         </div>
     )
+}
+
+// document props
+Topbar.displayName = 'Topbar'
+
+Topbar.propTypes = {
+    menuOpen: PropTypes.bool.isRequired,
+    setMenuOpen: PropTypes.func.isRequired
 }
 
 export default Topbar
