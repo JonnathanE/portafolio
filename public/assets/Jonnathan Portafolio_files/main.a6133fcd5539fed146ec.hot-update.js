f44b8f4b6a282e4c3382af402f5d88bbd5357b07
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-11-18 01:30:40 -0500

    React introduction page

---
+++ public/assets/Jonnathan Portafolio_files/main.a6133fcd5539fed146ec.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./src/components/intro/intro.scss":
+/*!*********************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-6-1!./node_modules/postcss-loader/src??postcss!./node_modules/resolve-url-loader??ref--5-oneOf-6-3!./node_modules/sass-loader/dist/cjs.js??ref--5-oneOf-6-4!./src/components/intro/intro.scss ***!
+  \*********************************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(true);
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".intro {\n  background-color: white;\n  display: flex;\n}\n.intro .left {\n  flex: 0.5 1;\n  overflow: hidden;\n}\n.intro .left .imgContainer {\n  width: 700px;\n  height: 700px;\n  background-color: crimson;\n  border-radius: 50%;\n  display: flex;\n  align-items: flex-end;\n}\n.intro .left .imgContainer img {\n  height: 90%;\n}\n.intro .right {\n  flex: 0.5 1;\n}", "",{"version":3,"sources":["webpack://src/components/intro/intro.scss"],"names":[],"mappings":"AAAA;EACI,uBAAA;EACA,aAAA;AACJ;AACI;EACI,WAAA;EACA,gBAAA;AACR;AACQ;EACI,YAAA;EACA,aAAA;EACA,yBAAA;EACA,kBAAA;EACA,aAAA;EACA,qBAAA;AACZ;AACY;EACI,WAAA;AAChB;AAII;EACI,WAAA;AAFR","sourcesContent":[".intro {\n    background-color: white;\n    display: flex;\n\n    .left {\n        flex: 0.5;\n        overflow: hidden;\n\n        .imgContainer {\n            width: 700px;\n            height: 700px;\n            background-color: crimson;\n            border-radius: 50%;\n            display: flex;\n            align-items: flex-end;\n\n            img {\n                height: 90%;\n            }\n        }\n    }\n\n    .right {\n        flex: 0.5;\n    }\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ __webpack_exports__["default"] = (___CSS_LOADER_EXPORT___);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=main.a6133fcd5539fed146ec.hot-update.js.map
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,21 @@ import './intro.scss'
 const Intro = () => {
     return (
         <div className='intro' id='intro'>
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jonnathan Damián Espinoza Erráez</h1>
+                    <h3>Web developer <span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
